d2a68b7ab4fa2923c957943b1c454eb158350ec2
newline-power-tools <doug@theironyard.com>
2017-08-15 14:51:40 +0000

    ada55d44-method_basics.zip

---
+++ method_basics/starter_files/main.rb
@@ -0,0 +1,48 @@
+# Complete this method so that it displays "Hello, " and the name local variable in the CLI.
+def greeting(name)
+  # add line here
+end
+
+
+# Complete this method so that it displays the half the number
+def halve(number)
+  # add code here
+end
+
+
+# Complete this method so that it displays an on sale price of an item.
+# The price decrease is in dollars, not percent
+def sale_price(original_price, price_decrease)
+  # add code here
+end
+
+
+# complete this method that takes someone's hometown as an argument and says "I hear [hometown] is gorgeous."
+def hometown_compliment(hometown)
+  # code goes here
+end
+
+
+# complete this method that displays the item and the price of the item.
+def item_information #code goes here
+  #code goes here
+end
+
+
+# complete this method for greeting someone with their name.  If the person is from Texas, we want to display "Howdy" instead of "Hello"
+def texan_greeting(name, state)
+  if true # replace true, code goes here
+    # code goes here
+  else
+    # code goes here
+  end
+end
+
+
+# create a method that changes the price based on time of day.
+# In the morning, we display the full price
+# In the afternoon, we display 80% of the full price
+# In the evening, we display 60% of the full price
+def price_by_time_of_day # code goes here
+  # code goes here
+end
